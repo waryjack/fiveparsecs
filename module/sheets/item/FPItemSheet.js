a48ce31cb831083477cbac2eb8bf4815e50d2571
Chris Clouser <waryjack@gmail.com>
2021-06-13 21:01:03 -0400

    framework for generating worlds, encounters

--- module/sheets/item/FPItemSheet.js
+++ module/sheets/item/FPItemSheet.js
@@ -1,3 +1,6 @@
+import { FPRollUtility } from "../../utility/FPRollUtility.js";
+import { FPProcGen } from "../../utility/FPProcGen.js";
+
 export class FPItemSheet extends ItemSheet {
 
     get template() {
@@ -19,6 +22,17 @@ export class FPItemSheet extends ItemSheet {
         });
     }
     
+    /**
+     * @override
+     */
+     activateListeners(html) {
+        super.activateListeners(html);
+        // Everything below here is only needed if the sheet is editable
+        if (!this.options.editable) return;
+
+        html.find('.gen-world').click(this._generateWorld.bind(this));
+
+    }
 
     getData() {
         const data = this.item.data;
@@ -49,5 +63,22 @@ export class FPItemSheet extends ItemSheet {
         }); 
 
         return eligibleCrew;
+    }
+
+    _generateWorld(e) {
+        e.preventDefault();
+        return ui.notifications.warn("Non functional until tables are prepared");
+        
+        return; // stubbing out until the 
+        return FPProcGen._generateWorld(this.item).then(o => {
+
+            let finalTraits = o.genTrait_1 + (o.genTrait_2 != "") ? ", " + o.genTrait_2 : "";
+            this.item.update({
+                              "data.data.traits" : finalTraits,
+                              "data.data.licensing": o.licensing,
+                              "name": o.genName
+                            });
+                            
+        });
     }
 }
