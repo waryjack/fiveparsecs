b25959b0096a6ffe511afba27fbb83764346b174
Chris Clouser <waryjack@gmail.com>
2021-06-19 08:02:02 -0400

    working on basic campaign turn tracker

--- module/campaign/FPCampaignTurn.js
+++ module/campaign/FPCampaignTurn.js
@@ -1,3 +1,14 @@
+/**
+ * Note: This is a potential option for sophisticated automation of the system.
+ * However, because it basically walks the player through the steps of the game,
+ * that endges uncomfortably close to reproducing the game content in the 
+ * foundry system. 
+ * 
+ * For now, this will be left as a sideline/future work, and 
+ * the system will stick with battles and jobs being Item types that the user
+ * creates, so the game *procedure* isn't reproduced here.
+ */
+
 import { FPBattle } from "./FPBattle.js";
 import { FPJob } from "./FPJob.js";
 import { FPWorld } from "./FPWorld.js";
